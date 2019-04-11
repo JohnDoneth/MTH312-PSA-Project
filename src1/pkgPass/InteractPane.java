15adec136b8000f74d52086c2ae240a16c651fe8
C-Low3 <RLCritchlow.III@gmail.com>
2019-04-11 15:06:21 -0400

    +button functionality +error checking

--- src1/pkgPass/InteractPane.java
+++ src1/pkgPass/InteractPane.java
@@ -1,7 +1,11 @@
 package pkgPass;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -19,6 +23,9 @@ public class InteractPane extends VBox{
 	//Password to be checked
 	private String passChk;
 	
+	//Number of found passwords
+	private int numPass;
+	
 	//Button 
 	private Button checkBtn; 
 	
@@ -32,6 +39,8 @@ public class InteractPane extends VBox{
 		
 		//initializing variables and fields
 		passChk = "Password123";
+		numPass = 0;
+		
 		aboveLbl = new Label("Enter a Password:");
 		belowLbl = new Label("We Found "+getNumPass(passChk) + 
 							 " other users with the same password");
@@ -40,7 +49,39 @@ public class InteractPane extends VBox{
 		passTF = new TextField();
 		passTF.setPromptText("password");
 		
+		Alert passError = new Alert(AlertType.ERROR);
+		
+		
 		getChildren().addAll(aboveLbl,passTF, checkBtn, belowLbl);
+		
+		//action listener
+		checkBtn.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        
+		    	//get password
+		    	passChk = passTF.getText();
+		        
+		    	//check validity of password
+		    	if(passChk.length() < 3 ){
+		    		passError.setContentText("Password must contain at least 3 characters");
+		    		passError.showAndWait();
+		    	}
+		    	else if(passChk.contains(" ")){
+		    		passError.setContentText("Password cannot contain spaces");
+		    		passError.showAndWait();
+		    	}	
+		    	else{
+		        //search
+		    	numPass = getNumPass(passTF.getText());
+		        checkBtn.setText(passTF.getText());
+		        
+		        
+		        //Update Findings
+		        belowLbl.setText("We Found "+ numPass + 
+							 " other users with the same password");
+		    	}
+		    }
+		});
 		
 	}
 	
@@ -50,6 +91,10 @@ public class InteractPane extends VBox{
 	private int getNumPass(String str){
 		
 		int numPass = 0;
+		if (str.length() == 3)
+			numPass = 1;
+		
+		
 			//FIXME Connect to Corbin's search here.
 		return numPass;
 	}
