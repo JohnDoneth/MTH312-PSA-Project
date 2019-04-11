d2d7270c36568ed567a71f76cd657b0881931df1
C-Low3 <RLCritchlow.III@gmail.com>
2019-04-11 11:49:10 -0400

    Skeleton GUI

---
+++ src1/pkgPass/InteractPane.java
@@ -0,0 +1,38 @@
+package pkgPass;
+
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class InteractPane extends VBox{
+
+	//Labels
+	private Label aboveLbl;
+	private Label belowLbl;
+	
+	//Text Field
+	private TextField passTF;
+	
+	/******************************************************************
+	 * Constructor that builds the Pane
+	 *****************************************************************/
+	public InteractPane(){
+		setPadding(new Insets(0, 25, 10, 10));
+		setSpacing(25);
+		setAlignment(Pos.CENTER);
+		
+		
+		aboveLbl = new Label("Enter a Password");
+		belowLbl = new Label("We Found 32 other users with the same password");
+	
+		passTF = new TextField();
+		
+		
+		getChildren().addAll(aboveLbl,passTF, belowLbl);
+		
+	}
+	
+}
---
+++ src1/pkgPass/PasswordGUI.java
@@ -0,0 +1,64 @@
+package pkgPass;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class PasswordGUI extends Application{
+
+	
+	/******************************************************************
+	 * The Main Method
+	 * @param args The command line
+	 *****************************************************************/
+	public static void main (String[] args){
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//Setting title
+		primaryStage.setTitle("For Sure the final version");
+		
+		//Creating the necessary panes
+		BorderPane  mainPn = new BorderPane();
+		InteractPane InteractPn = new InteractPane();
+		
+		//creating the MenuBar
+		MenuBar menuBar = new MenuBar();
+		Menu fileMenu = new Menu("File");
+		MenuItem newItm = new MenuItem("New");
+		
+		
+		//Setting up the menu bar
+	    fileMenu.getItems().addAll(newItm);
+	    menuBar.getMenus().add(fileMenu);
+	    
+	    //Setting up Main Pane
+		mainPn.setTop(menuBar);
+		mainPn.setCenter(InteractPn);
+		
+		//Setting up stage
+		Scene mainScene = new Scene(mainPn, 800, 600);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
+
+		//Ensure system close when the red X is hit
+		primaryStage.setOnCloseRequest(e -> {
+	        Platform.exit();
+	        System.exit(0);
+	    });
+		
+	}
+
+	
+}
+
+
+	
