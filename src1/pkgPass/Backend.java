5efa26a1de3c0af65df46c688799fbc438c896e4
Corbin Bremmeyr <bremmeyc@mail.gvsu.edu>
2019-04-11 13:02:39 -0400

    setup seraching backend in GUI

---
+++ src1/pkgPass/Backend.java
@@ -0,0 +1,83 @@
+package pkgPass;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Backend {
+
+    private String basePath;
+    
+    public Backend(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public Backend() {
+        this.basePath = "passwords/full_counted/output/";
+    }
+
+    public int search(String password) throws Exception {
+
+        String path = null;
+        try{
+            // Make path to file
+            path = basePath + password.charAt(0) + "/" + password.charAt(1) + "/" + password.charAt(2);
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            throw new Exception();
+        }
+
+        Scanner scan = null;
+        try{
+
+            // Open file to search
+            scan = new Scanner(new File(path));
+        }
+        catch(Exception e) {
+            throw new Exception();
+        }
+
+        String temp = null;
+        
+        // Linear search for password
+        while(scan.hasNextLine()) {
+
+            // Check if this line is password being searched for
+            temp = scan.nextLine();
+
+
+            System.out.println(temp);
+
+
+            int index = -1;
+            if(temp.matches(password + ":\\d+")) {
+                    
+                // Get index of start of count value
+                for(int i = temp.length()-1; i >= 0; --i) {
+                    if(temp.charAt(i) == ':') {
+                        index = i+1;
+                        break;
+                    }
+                }
+
+                // Read out count value
+                if(index != -1) {
+                    return Integer.parseInt(temp.substring(index));
+                }
+            }
+        }
+
+        // Password not found
+        return -1;
+    }
+
+//    public static void main(String[] args) {
+//        Backend b = new Backend();
+//        try {
+//            System.out.println("Result: " + b.search(args[0]));
+//        }
+//        catch(Exception e) {
+//            System.out.println("Oops");
+//        }
+//    }
+
+}
--- src1/pkgPass/InteractPane.java
+++ src1/pkgPass/InteractPane.java
@@ -9,30 +9,36 @@ import javafx.scene.layout.VBox;
 
 public class InteractPane extends VBox{
 
-	//Labels
-	private Label aboveLbl;
-	private Label belowLbl;
-	
-	//Text Field
-	private TextField passTF;
-	
-	/******************************************************************
-	 * Constructor that builds the Pane
-	 *****************************************************************/
-	public InteractPane(){
-		setPadding(new Insets(0, 25, 10, 10));
-		setSpacing(25);
-		setAlignment(Pos.CENTER);
-		
-		
-		aboveLbl = new Label("Enter a Password");
-		belowLbl = new Label("We Found 32 other users with the same password");
-	
-		passTF = new TextField();
-		
-		
-		getChildren().addAll(aboveLbl,passTF, belowLbl);
-		
-	}
-	
+    //Labels
+    private Label aboveLbl;
+    private Label belowLbl;
+    
+    //Text Field
+    private TextField passTF;
+    
+    /******************************************************************
+     * Constructor that builds the Pane
+     *****************************************************************/
+    public InteractPane(){
+        setPadding(new Insets(0, 25, 10, 10));
+        setSpacing(25);
+        setAlignment(Pos.CENTER);
+
+        // New backend for searching
+        String basePath = "../../passwords/full_counted/output/";
+        Backend b = new Backend(basePath);  
+
+        // To search:
+//        b.search(searchSting);
+        
+        aboveLbl = new Label("Enter a Password");
+        belowLbl = new Label("We Found 32 other users with the same password");
+    
+        passTF = new TextField();
+        
+        
+        getChildren().addAll(aboveLbl,passTF, belowLbl);
+        
+    }
+    
 }
